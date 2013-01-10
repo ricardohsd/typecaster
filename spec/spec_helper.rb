3a474a20dfb44606eb85107d4b65f0b0a158c2da
Ricardo H <ricardohsd@gmail.com>
2013-01-10 09:57:58 -0200

    Move typecasters used in the tests to the spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,22 @@
 require 'rspec'
 require 'typecaster'
+
+module StringTypecaster
+  def self.call(value, options)
+    value.to_s.ljust(options[:size], " ")
+  end
+
+  def self.parse(value)
+    value.strip
+  end
+end
+
+module IntegerTypecaster
+  def self.call(value, options)
+    value.to_s.rjust(options[:size], "0")
+  end
+
+  def self.parse(value)
+    value.to_f
+  end
+end
